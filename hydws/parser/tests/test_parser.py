21d5db6d000be0f08f4a023bf6d300c0e969cde5
Nicolas Schmid <nicolas@breiten.ch>
2023-09-28 13:47:56 +0200

    test: add tests for new HYDJSONParser

--- hydws/parser/tests/test_parser.py
+++ hydws/parser/tests/test_parser.py
@@ -3,54 +3,92 @@ import os
 from copy import deepcopy
 
 import pandas as pd
+import pytest
 
-from hydws.parser.parser import HYDJSONParser
+from hydws.parser.parser import (HYDJSONParser, SectionHydraulics,
+                                 empty_section_metadata, is_valid_uuid)
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def test_parser():
-    parser = HYDJSONParser()
+@pytest.fixture
+def df():
+    return pd.read_parquet(os.path.join(DIRPATH, 'hydraulics.parquet'))
 
+
+@pytest.fixture
+def hydjson():
     with open(os.path.join(DIRPATH, 'hydraulics.json'), 'rb') as f:
-        data = json.load(f)
-
-    parser.load_borehole_json(data)
-    df = parser.get_hydraulics_dataframe_by_name('ST1_section_02')
-
-    df2 = pd.read_parquet(os.path.join(DIRPATH, 'hydraulics.parquet'))
-
-    pd.testing.assert_frame_equal(df, df2)
+        hydjson = json.load(f)
+    return hydjson
 
 
-def test_new_parser():
-    parser = HYDJSONParser()
-    test_df = pd.read_parquet(os.path.join(DIRPATH, 'hydraulics.parquet'))
-
-    with open(os.path.join(DIRPATH, 'hydraulics.json'), 'rb') as f:
-        data = json.load(f)
-
-    parser.load_borehole_json(data)
-
-    borehole_metadata = deepcopy(data)
-    borehole_metadata['sections'][1].pop('hydraulics')
-    section_metadata = deepcopy(data['sections'][1])
-    section_metadata.pop('hydraulics')
-
-    borehole_id = borehole_metadata['publicid']
-    section_id = section_metadata['publicid']
-    section_df = parser.hydraulics[section_id]
-
-    # test metadata and hydraulics access
-    pd.testing.assert_frame_equal(section_df, test_df)
-    assert section_metadata == parser.metadata[section_id]
-    assert borehole_metadata == parser.metadata[borehole_id]
+@pytest.fixture
+def metadata(hydjson):
+    metadata = deepcopy(hydjson['sections'][1])
+    metadata.pop('hydraulics')
+    return metadata
 
 
-def test_general():
+class TestHYDJSONParser:
+    def test_init(self, hydjson, df):
 
-    mydict = {'a': [1, 2, 3]}
-    mydict['b'] = mydict['a']
+        parser = HYDJSONParser(hydjson)
 
-    mydict['a'].append(4)
-    print(mydict)
+        hydjson['sections'][1].pop('hydraulics')
+        hydraulics_id = hydjson['sections'][1]['publicid']
+        section_json = hydjson.pop('sections')[0]
+        section_json.pop('hydraulics')
+
+        assert parser.borehole == hydjson
+        pd.testing.assert_frame_equal(parser[hydraulics_id].hydraulics, df)
+
+        assert parser[section_json['publicid']].metadata == section_json
+        assert parser.nloc[section_json['name']].metadata == section_json
+
+        for key, value in parser.items():
+            assert parser[key] == value
+
+    def test_to_json(self, hydjson):
+        parser = HYDJSONParser(hydjson)
+        borehole_json = parser.to_json()
+        assert borehole_json == hydjson
+
+
+class TestSectionHydraulics:
+    def test_metadata(self):
+        hydraulics = SectionHydraulics()
+        assert is_valid_uuid(hydraulics.metadata['publicid'])
+        del hydraulics.metadata['publicid']
+        metadata = empty_section_metadata()
+        del metadata['publicid']
+        assert hydraulics.metadata == metadata
+
+    def test_load_hydraulic_dataframe(self, df):
+        hydraulics = SectionHydraulics()
+        hydraulics.load_hydraulic_dataframe(df)
+        pd.testing.assert_frame_equal(hydraulics.hydraulics, df)
+
+    def test_load_hydraulic_json(self, hydjson, df):
+        hydraulics = SectionHydraulics()
+        hydraulics.load_hydraulic_json(
+            hydjson['sections'][1]['hydraulics'])
+        pd.testing.assert_frame_equal(hydraulics.hydraulics, df)
+
+    def test_load_section_json(self, hydjson, df, metadata):
+        hydraulics = SectionHydraulics()
+
+        hydraulics._from_json(hydjson['sections'][1])
+        pd.testing.assert_frame_equal(hydraulics.hydraulics, df)
+        assert hydraulics.metadata == metadata
+
+    def test_init(self, hydjson, df, metadata):
+        hydraulics = SectionHydraulics(hydjson['sections'][1])
+        pd.testing.assert_frame_equal(hydraulics.hydraulics, df)
+        assert hydraulics.metadata == metadata
+
+    def test_to_json(self, hydjson, df):
+        hydraulics = SectionHydraulics(hydjson['sections'][1])
+        hydraulics_json = hydraulics.to_json()
+        assert hydraulics_json == hydjson['sections'][1]
+        pd.testing.assert_frame_equal(hydraulics.hydraulics, df)
